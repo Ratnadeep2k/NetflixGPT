89f717fa237b207bfa3cdf41a4da6cd4f72ce04f
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-04-05 12:27:51 +0530

    Min Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
+import { Body } from './components/Body';
 
 function App() {
   return (
    <>
-   <div className='text-3xl font-bold text-red'>
-     Hey ,DeV
-   </div>
-     
+    <Body/>
    </>
   );
 }
